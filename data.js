acf752c70c44eb9db050c696d9922832a897d594
s-i-d-d-i-s <68241942+s-i-d-d-i-s@users.noreply.github.com>
2021-02-16 12:33:18 +0530

    Updated Stuff

---
+++ data.js
@@ -0,0 +1,13 @@
+data = {
+	"current_session": "2018",
+	"current_team": "X.0",
+	"current_team_pic": "https://st3.depositphotos.com/10654668/17465/i/1600/depositphotos_174651896-stock-photo-teamwork.jpg",
+	"latest_event_title" : "Event Name",
+	"latest_event_banner":"https://image.shutterstock.com/shutterstock/photos/1551185741/display_1500/stock-vector-music-event-banner-design-template-for-digital-advertisement-media-and-print-modern-gradient-1551185741.jpg",
+	"current_ranking_batch":"202X",
+	"current_ranking_batch_image":"https://image.shutterstock.com/shutterstock/photos/510352774/display_1500/stock-photo-top-performers-510352774.jpg",
+	"contact_email":"codingclub.bitp@bitmesra.ac.in",
+	"contact_no":"+91 98765-43210",
+}
+
+
